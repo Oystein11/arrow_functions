9c0e5940a534a4220b9e10b94a75e9a8a989424f
Øystein <oystein.koder@gmail.com>
2023-11-09 14:46:14 +0100

    changes

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const boredCard = document.querySelector (".bored_card");
-
 function get_info () {
-    fetch ("https://www.boredapi.com/api/activity")
+    fetch ("https://pokeapi.co/api/v2/pokemon/ditto")
     .then ((response) => response.json())
-    .then ((data) => {
-        console.log (data);
-    })
-    .catch ((error) => console.log (error));
+    .then (data => show_data (data.results));
+
+    show_data = data => {
+        const pokemon_info = document.querySelector (".pokemon_info");
+        data.for_each (data =>{
+            const pokemon_element = document.createElement ("p");
+            pokemon_element.innerText = "Pokemon Traits: ${ditto.name}";
+            pokemon_info.append (pokemon_element);
+        });
+    }
 }
 
 get_info();
+pokemon_info.textContent = get_info;
