17c0e64b5ddc3e5f346cb93b2b693e68214457b9
Øystein <oystein.koder@gmail.com>
2023-11-10 09:09:22 +0100

    changes

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+const pokemon_info = document.querySelector (".pokemon_info");
+const pokemon_element = document.createElement ("p");
+
 function get_info () {
     fetch ("https://pokeapi.co/api/v2/pokemon/ditto")
     .then ((response) => response.json())
-    .then (data => show_data (data.results));
-
-    show_data = data => {
-        const pokemon_info = document.querySelector (".pokemon_info");
-        data.for_each (data =>{
-            const pokemon_element = document.createElement ("p");
-            pokemon_element.innerText = "Pokemon Traits: ${ditto.name}";
-            pokemon_info.append (pokemon_element);
-        });
-    }
+    .then (data => {
+        console.log (data);
+        show_data (data.results)});
 }
 
+show_data = data => {
+        pokemon_element.innerText = "Pokemon Traits: ${ditto.name}";
+        pokemon_info.append (pokemon_element);
+        pokemon_info.textContent = get_info;
+};
+
 get_info();
-pokemon_info.textContent = get_info;
